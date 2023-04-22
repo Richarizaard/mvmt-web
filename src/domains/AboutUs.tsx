09e3ae73183aa84f56c756d7b5bb202ddcebe467
Richard Lee <richardlee1357@gmail.com>
2023-04-21 23:04:30 -0700

    Fix case

--- src/domains/AboutUs.tsx
+++ src/domains/AboutUs.tsx
@@ -2,7 +2,7 @@ import PageIntroCard from 'components/PageIntroCard'
 import home from 'assets/home.png'
 
 const AboutUs = () => {
-  return <PageIntroCard typedStrings={['About Us']} img={home} />
+  return <PageIntroCard typedStrings={['About us']} img={home} />
 }
 
 export default AboutUs
--- src/domains/ContactUs.tsx
+++ src/domains/ContactUs.tsx
@@ -4,7 +4,7 @@ import home from 'assets/home.png'
 const ContactUs = () => {
   return (
     <PageIntroCard
-      typedStrings={['Contact Us']}
+      typedStrings={['Contact us']}
       desc="Hey there! Do you have a question? Feedback for us? Maybe you have a prayer request? Please fill out the form below and we will get back with you asap."
       img={home}
     />
