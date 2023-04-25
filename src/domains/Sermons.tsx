68a2f566b3df44ca56664ecf554a567823ee0549
Richard Lee <richardlee1357@gmail.com>
2023-04-25 15:27:19 -0700

    Remove homepage | add carousel

--- src/domains/Sermons.tsx
+++ src/domains/Sermons.tsx
@@ -2,7 +2,11 @@ import PageIntroCard from 'components/PageIntroCard'
 import home from 'assets/home.png'
 
 const Sermons = () => {
-  return <PageIntroCard typedStrings={['Sermons']} img={home} />
+  return (
+    <>
+      <PageIntroCard typedStrings={['Sermons']} img={home} />
+    </>
+  )
 }
 
 export default Sermons
